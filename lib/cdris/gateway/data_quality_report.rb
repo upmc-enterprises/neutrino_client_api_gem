e707822c61a580af3c597c9ffb1214760f35a6cf
Logan Stahl <stahll2@upmc.edu>
2019-07-02 16:38:50 -0400

    [CDRIS-4761] Data Quality Report - Remove manual update and necessary infrastructure

--- lib/cdris/gateway/data_quality_report.rb
+++ lib/cdris/gateway/data_quality_report.rb
@@ -6,17 +6,7 @@ module Cdris
     class DataQualityReport < Cdris::Gateway::Requestor
       private_class_method :new
       class << self
-
-        # Update the data quality report
-        # Create a new one if there is no existing report
-        # @param [Hash] options specify query values
-        # @return [Hash] CDRIS response
-        def create_or_update(options = {})
-          path = "#{api}/reports/data-quality/summary/generate"
-          request(path, options.merge!(method: :post)).
-            if_400_raise(Cdris::Gateway::Exceptions::BadRequestError.new()).to_hash
-        end
-
+        
         # Get the summary of data quality
         # @param [Hash] options specify query values
         # @return [Hash] CDRIS response
--- spec/cdris/gateway/data_quality_report_spec.rb
+++ spec/cdris/gateway/data_quality_report_spec.rb
@@ -10,41 +10,6 @@ describe Cdris::Gateway::DataQualityReport do
     Cdris::Api::Client.config = TestConfig.to_hash
   end
 
-  describe 'self.create_or_update' do
-
-    let(:path) { '/api/v1/reports/data-quality/summary/generate' }
-    let(:response_message) { { 'message'=>'The system is updating the report' }  }
-
-    before(:each) do
-      FakeWeb.register_uri(
-        :post,
-        'http://testhost:4242/api/v1/reports/data-quality/summary/generate?user%5Bextension%5D=spameggs&user%5Broot%5D=foobar',
-        body: response_message.to_json , status: ['200', 'OK'])
-    end
-
-    it 'performs a post request' do
-      expect(Cdris::Gateway::Requestor).to receive(:request).with(path, { method: :post }).and_call_original
-      expect(described_class.create_or_update).to eq(response_message)
-    end
-
-    context 'when the server returns a 400 error' do
-
-      before(:each) do
-        FakeWeb.register_uri(
-          :post,
-          'http://testhost:4242/api/v1/reports/data-quality/summary/generate?user%5Bextension%5D=spameggs&user%5Broot%5D=foobar',
-          body: 'Bad Request', status: ['400', 'Bad Request'])
-      end
-
-      it 'raises a bad request error' do
-        expect(Cdris::Gateway::Requestor).to receive(:request).with(path, { method: :post }).and_call_original
-        expect { described_class.create_or_update }.to raise_error(Cdris::Gateway::Exceptions::BadRequestError)
-      end
-
-    end
-
-  end
-
   describe 'self.summary' do
 
     let(:path) { '/api/v1/reports/data-quality/summary' }
