e81291dfef19d806c881b1906b9f02aad4230d61
Yimin Nie <niexyx@upmc.edu>
2020-05-26 14:13:49 -0400

    [CDRIS-5550]Remove millisecond monkeypatch

--- spec/neutrino/helpers/utc_iso8601_shared_examples.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-shared_examples 'a UTC ISO8601 formatted Date/Time with millisecond resolution' do
-  it { should match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/) }
-  it { should match(/T[\d\:\.]+Z$/) }
-  it { should match(/T[\d\:]+\.(\d+).$/) }
-end
